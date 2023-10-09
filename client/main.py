500f4082579508e5cc44e86da67e74429c1d86c3
JasonCubed132 <amcb2002@googlemail.com>
2023-10-09 23:35:33 +0100

    add terminal to client

--- client/main.py
+++ client/main.py
@@ -3,31 +3,49 @@ from websockets.sync.client import connect
 
 def main():
     with connect("ws://localhost:1234") as websocket:
-        turn = {
-            "action": "turn",
-            "payload": {
-                "player": "X",
-                "x": 2,
-                "y": 1
-            }
-        };
-        websocket.send(json.dumps(turn))
-        message = websocket.recv()
-        print(f"Recieved: {message}")
+        while True:
+            line = input("> ")
 
-        get_state = {
-            "action": "get_board"
-        }
-        websocket.send(json.dumps(get_state))
-        message = websocket.recv()
-        print(f"Recieved: {message}")
+            if len(line) == 0:
+                print("No data entered")
+                continue
 
-        get_state = {
-            "action": "get_winner"
-        }
-        websocket.send(json.dumps(get_state))
-        message = websocket.recv()
-        print(f"Recieved: {message}")
+            data = line.split(" ")
+
+            action = data[0]
+
+            if action == "turn":
+                x = int(data[1])
+                y = int(data[2])
+                player = data[3]
+
+                turn = {
+                    "action": "turn",
+                    "payload": {
+                        "player": player,
+                        "x": x,
+                        "y": y
+                    }
+                };
+                websocket.send(json.dumps(turn))
+                message = websocket.recv()
+                print(f"Recieved: {message}")
+
+            elif action == "board":
+                get_state = {
+                    "action": "get_board"
+                }
+                websocket.send(json.dumps(get_state))
+                message = websocket.recv()
+                print(f"Recieved: {message}")
+
+            elif action == "winner":
+                get_state = {
+                    "action": "get_winner"
+                }
+                websocket.send(json.dumps(get_state))
+                message = websocket.recv()
+                print(f"Recieved: {message}")
 
 if __name__ == "__main__":
     main()
